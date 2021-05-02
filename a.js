df77ff13c076d8645b4df65fdc22d0a409c9d90e
Jeremy Evan <16620367@mahasiswa.itb.ac.id>
2021-05-03 00:50:53 +0700

    make the contact form work

--- a.js
+++ a.js
@@ -4,3 +4,29 @@ function flipCard() {
   this.classList.toggle("flip");
 }
 cards.forEach((card) => card.addEventListener("click", flipCard));
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbx4m8UwrIk8YTocAp37LGByR2mIw3uIsyYKtVc2ilTJzqBn5Z0F2JdPYFsZHUwPxSKOQw/exec";
+const form = document.forms["contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit diclick
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      // tampilkan alert
+      myAlert.classList.toggle("d-none");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
